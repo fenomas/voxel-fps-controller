9b8f1ac491c84fa255807efe96c69000996fa442
andyhall <andy@fenomas.com>
2015-01-28 16:28:12 +0900

    Air jumps and jump force over time
    Implements options 'airJumps' ,'jumpForce', 'jumpTime'
    Set airJumps >= 1 to allow air jumping.
    Jumping mechanics - 'jumpImpulse' is applied once per jump,
    'jumpForce' is applied as long as the jump button stays
    pressed, up to 'jumpTime' milliseconds.

--- index.js
+++ index.js
@@ -19,8 +19,13 @@ var defaults = {
   , responsiveness: 20
   , runningFriction: 0
   , standingFriction: 20
-  , jumpImpulse: 15
+
   , airMoveMult: 0.3
+  , jumpImpulse: 8
+  , jumpForce: 11
+  , jumpTime: 350 // ms
+  , airJumps: 1
+
   , crouchMoveMult: 0.6
   , sprintMoveMult: 1.3
   , inverseY: true
@@ -41,18 +46,24 @@ function FPSController(game, opts) {
 
   // engine setup
   opts = extend( {}, defaults, opts )
-  this.moveForce = opts.moveForce
+  this.moveForce      = opts.moveForce
   this.responsiveness = opts.responsiveness
-  this.jumpImpulse = opts.jumpImpulse
-  this.airMoveMult = opts.airMoveMult
+  this.jumpImpulse    = opts.jumpImpulse
+  this.jumpForce      = opts.jumpForce
+  this.jumpTime       = opts.jumpTime
+  this.airJumps       = opts.airJumps
+  this.airMoveMult    = opts.airMoveMult
   this.crouchMoveMult = opts.crouchMoveMult
   this.sprintMoveMult = opts.sprintMoveMult
-  this.inverseY = opts.inverseY
-  this.rotationScale = opts.rotationScale
+  this.inverseY       = opts.inverseY
+  this.rotationScale  = opts.rotationScale
   this.standingFriction = opts.standingFriction
   this.runningFriction = opts.runningFriction
-  this.maxSpeed = opts.maxSpeed
-
+  this.maxSpeed       = opts.maxSpeed
+  
+  this._jumping = false
+  this._airjumps = 0
+  this._currjumptime = 0
 }
 
 
@@ -72,30 +83,58 @@ proto.setCamera = function(camera) {
 
 
 
+var state, target, onGround
+, dx, dy, rotX, rotY, speed
+, m    = vec3.create()
+, push = vec3.create()
+, pushLen, canPush, pushAmt
+
+
 proto.tick = function(dt) {
   if(!this._target || !this._camera) return
 
-  var state = this.state
-  var target = this._target
-  var onGround = (target.atRestY() < 0)
+  state = this.state
+  target = this._target
+  onGround = (target.atRestY() < 0)
 
 
   // Rotation: translate dx/dy inputs into y/x axis camera angle changes
-  var dx = this.rotationScale * state.dy * ((this.inverseY) ? -1 : 1)
-  var dy = this.rotationScale * state.dx
+  dx = this.rotationScale * state.dy * ((this.inverseY) ? -1 : 1)
+  dy = this.rotationScale * state.dx
   // normalize/clamp/update
-  var rotX = clamp( this._camera.rotationX + dx, halfpi )
-  var rotY = (this._camera.rotationY + dy) % twopi
+  rotX = clamp( this._camera.rotationX + dx, halfpi )
+  rotY = (this._camera.rotationY + dy) % twopi
   this._camera.rotationX = rotX
   this._camera.rotationY = rotY
 
-  // Jumping
-  if (state.jump && onGround) {
-    target.applyImpulse( [0, this.jumpImpulse, 0] )
+  // jumping
+  var canjump = (onGround || this._airjumps < this.airJumps)
+  if (onGround) {
+    this._jumping = false
+    this._airjumps = 0
+  }
+  if (state.jump) {
+    if (this._jumping) { // continue previous jump
+      if (this._currjumptime > 0) {
+        var jf = this.jumpForce
+        if (this._currjumptime < dt) jf *= this._currjumptime/dt
+        target.applyForce( [0, jf, 0] )
+        this._currjumptime -= dt
+      }
+    } else if (canjump) { // start new jump
+      this._jumping = true
+      if (!onGround) this._airjumps++
+      this._currjumptime = this.jumpTime
+      target.applyImpulse( [0, this.jumpImpulse, 0] )
+      // clear downward velocity on airjump
+      if (!onGround && target.velocity[1]<0) target.velocity[1] = 0
+    }
+  } else {
+    this._jumping = false
   }
 
   // Movement: determine local direction of desired movement
-  var m = vec3.create()
+  vec3.set( m, 0, 0, 0 )
   if (state.backward) m[2] += 1
   if (state.forward)  m[2] -= 1
   if (state.right)    m[0] += 1
@@ -105,7 +144,7 @@ proto.tick = function(dt) {
   if (m[0] !== 0 || m[2] !== 0) {
     // convert to world coords and scale to desired movement vector
     vec3.rotateY( m, m, [0,0,0], -rotY )
-    var speed = this.maxSpeed
+    speed = this.maxSpeed
     if (state.sprint)  speed *= this.sprintMoveMult
     if (state.crouch)  speed *= this.crouchMoveMult
     vec3.scale( m, m, speed )
@@ -113,19 +152,18 @@ proto.tick = function(dt) {
     // push vector to achieve desired speed & dir
     // following code to adjust 2D velocity to desired amount is patterned on Quake: 
     // https://github.com/id-Software/Quake-III-Arena/blob/master/code/game/bg_pmove.c#L275
-    var push = vec3.create()
     vec3.subtract( push, m, target.velocity )
     push[1] = 0
-    var pushLen = vec3.length(push)
+    pushLen = vec3.length(push)
     vec3.normalize(push, push)
 
     if (pushLen > 0) {
       // pushing force vector
-      var canPush = this.moveForce
+      canPush = this.moveForce
       if (!onGround)  canPush *= this.airMoveMult
 
       // apply final force
-      var pushAmt = this.responsiveness * pushLen
+      pushAmt = this.responsiveness * pushLen
       if (canPush > pushAmt) canPush = pushAmt
 
       vec3.scale( push, push, canPush )
@@ -158,9 +196,6 @@ proto.tick = function(dt) {
   }
 
 }
-
-
-
 
 
 
